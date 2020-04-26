2ee0986a9d280a3ef2ef4cfdf186b370613245f4
EC2 Default User <ec2-user@ip-172-31-89-41.ec2.internal>
2020-04-26 10:14:36 +0000

    Added skills up and running

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,12 +1,19 @@
 class SkillsController < ApplicationController
+    
     def create
         @candidate = Candidate.find(params[:candidate_id])
         @skill = @candidate.skills.create(skill_params)
         redirect_to candidate_path(@candidate)
     end
     
-    private
+    def destroy
+        @candidate = Candidate.find(params[:candidate_id])
+        @skill = @candidate.skills.find(params[:id]) 
+        @skill.destroy
+        redirect_to candidate_path(@candidate)
+    end
     
+    private
     # For security purpose and what attrs are supposed to be saved to our model
     def skill_params
         params.require(:skill).permit(:name, :experience, :typeOfSkill)
